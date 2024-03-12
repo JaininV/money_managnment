2f6ddb49459b04880ff67aed46d76617b358309a
JaininV <jaininvekariya@gmail.com>
2024-03-12 15:00:03 -0400

    Generate token successfuly

--- models/login.py
+++ models/login.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request, url_for
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, verify_jwt_in_request
+
 
 from db_connection import connection, cursor
 import datetime
@@ -21,12 +22,10 @@ def loginUserApi(data):
             connection.commit()
 
             if results is not None:
-                try:
+                try:    
                     token_gen = create_access_token(identity=userid)
 
-                    return ({
-                        'token': token_gen
-                    })
+                    return jsonify(access_token=token_gen)
                 except Exception as e:
                     return f"Error: {str(e)}"
                 
@@ -40,6 +39,8 @@ def loginUserApi(data):
         return f"Error: {str(e)}"
 
 def loginCheckApi(data):
+    verify_jwt_in_request()
     current_user = get_jwt_identity()
-    return current_user
+    print(current_user)
+    return 'current_user'
  
