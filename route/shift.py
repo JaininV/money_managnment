5ad6471d27bf90723faf59d0d1e236051b7e0ef3
JaininV <jaininvekariya@gmail.com>
2024-03-21 15:09:12 -0400

    Remove unnecessary comments

--- route/shift.py
+++ route/shift.py
@@ -6,13 +6,10 @@ shift_blueprint = Blueprint('shifts', __name__)
 
 @shift_blueprint.route('/shift/add', methods=['POST'])
 def add_shift_api():
-    # Your API logic here
     data = addShift()
     return data
 
 @shift_blueprint.route('/shift/edit', methods=['POST'])
 def add_shift_api():
-    # Your API logic here
     data = updateShitTime()
-    # function retuen
     return data
