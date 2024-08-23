b548d264cfefcbdf85dec46b075c3d32ccb0b7f2
JaininV <jaininvekariya@gmail.com>
2024-08-23 14:55:32 -0400

    route done

--- route/expense.py
+++ route/expense.py
@@ -1,30 +1,13 @@
 from flask import Blueprint, jsonify
-from controller.shift import getShiftData, addShift, updateShitTime, deleteShift
+from controller.expense import getShiftData, addShift, updateShitTime, deleteShift
 
-shift_blueprint = Blueprint('expense', __name__)
+expense_blueprint = Blueprint('expense', __name__)
  
 
-@shift_blueprint.route('/shift/add', methods=['POST'])
+@expense_blueprint.route('/expense/add', methods=['POST'])
 def add_shift_api():
     try:
         data = addShift()
         return data 
-    except Exception as err:
-        return f"Error: {err}"
-
-@shift_blueprint.route('/shift/edit', methods=['PUT'])
-def update_shift_api():
-    try:
-        data = updateShitTime()
-        return data
-    except Exception as err:
-        return f"Error: {err}"
-    
-# Delete shift
-@shift_blueprint.route('/shift/delete', methods=['DELETE'])
-def delete_shift_api():
-    try:
-        data = deleteShift()
-        return data
     except Exception as err:
         return f"Error: {err}"
--- server.py
+++ server.py
@@ -12,6 +12,7 @@ from route.login import login_blueprint
 from route.job import job_blueprint
 from route.shift import shift_blueprint
 from route.income import income_blueprint
+from route.expense import expense_blueprint
 
 
 app = flask.Flask(__name__, template_folder='D:/D/Certification/Projects/Updated parking slot/public/', static_folder="static")
@@ -31,6 +32,7 @@ app.register_blueprint(login_blueprint, url_prefix='/api')
 app.register_blueprint(job_blueprint, url_prefix='/api')
 app.register_blueprint(shift_blueprint, url_prefix='/api')
 app.register_blueprint(income_blueprint, url_prefix='/api')
+app.register_blueprint(expense_blueprint, url_prefix='/api')
 
 #connect server
 try:
