e7475979233df2e51d04d1a64caa0f98d3071ce8
JaininV <jaininvekariya@gmail.com>
2024-08-25 17:15:27 -0400

    code done

--- youtube/demo.py
+++ youtube/demo.py
@@ -14,7 +14,14 @@ def split_video(inp, out_dr, duration=60):
     for i in range(clip):
         start_time = i * duration
         end_time = min((i+1)*duration, video_len)
-        
+
+        sub_clip = video.subclip(start_time, end_time)
+        out_file = os.path.join(out_dr, f'clip_{i+1}.mp4')
+
+        sub_clip.write_videofile(out_file, codec="libx264", audio_codec="aac")
+        print(f"Saved {out_file}")
+
+    print("Video splitting")        
 
 inp = "C:/Users/Owner/Videos/Captures/Mood mappers - Brave 2023-07-19 23-08-04.mp4"
 dir = "D:/D/Certification/Projects/Money handle/youtube/clips"
