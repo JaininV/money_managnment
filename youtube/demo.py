34c7a818861268fc1c27e0e7150e947c692f483a
JaininV <jaininvekariya@gmail.com>
2024-08-25 17:00:46 -0400

    create function to split video

--- youtube/demo.py
+++ youtube/demo.py
@@ -1,2 +1,13 @@
 from moviepy.video.io.VideoFileClip import VideoFileClip
 import os
+
+def split_video(inp, out_dr, duration=60):
+    # Load video
+    video = VideoFileClip(inp)
+
+    video_len = int(video.duration)
+    print(video_len)
+
+inp = "D:/D/Certification/Projects/Money handle/youtube/yt1s.com - 4K Planet Earth Spinning in Space  Free HD Videos  No Copyright_1080p.mp4"
+dir = "D:/D/Certification/Projects/Money handle/youtube/clips"
+split_video(inp, dir, )
